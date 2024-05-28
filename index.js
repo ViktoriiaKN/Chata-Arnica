7253ae9e9d308c94f8f68e585ab00a003a53d15a
Viktoriia Nykyforova <113258083+ViktoriiaKN@users.noreply.github.com>
2024-05-28 20:25:23 +0200

    dark-mode

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 // https://dev.to/ananyaneogi/create-a-dark-light-mode-switch-with-css-variables-34l8
 
-const toggleSwitch = document.querySelector(
-  '.theme-switch input[type="checkbox"]',
-);
+document.addEventListener('DOMContentLoaded', () => {
+  const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
-const switchTheme = (e) => {
-  if (e.target.checked) {
-    document.documentElement.setAttribute('data-theme', 'dark');
-  } else {
-    document.documentElement.setAttribute('data-theme', 'light');
+  const switchTheme = (e) => {
+    if (e.target.checked) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+      localStorage.setItem('theme', 'dark'); // Save theme to localStorage
+    } else {
+      document.documentElement.setAttribute('data-theme', 'light');
+      localStorage.setItem('theme', 'light'); // Save theme to localStorage
+    }
+  };
+
+  toggleSwitch.addEventListener('change', switchTheme, false);
+
+  // Check local storage for theme preference
+  const currentTheme = localStorage.getItem('theme');
+  if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'dark') {
+      toggleSwitch.checked = true;
+    }
   }
-};
-
-toggleSwitch.addEventListener('change', switchTheme, false);
-
-const currentTheme = localStorage.getItem('theme');
-if (currentTheme) {
-  document.documentElement.setAttribute('data-theme', currentTheme);
-  if (currentTheme === 'dark') {
-    toggleSwitch.checked = true;
-  }
-}
+});
