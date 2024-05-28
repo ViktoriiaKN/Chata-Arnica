2211507cd0a26584fa3490dc53cd21455a3a5417
Viktoriia Nykyforova <113258083+ViktoriiaKN@users.noreply.github.com>
2024-05-28 19:55:12 +0200

    git push

--- index.js
+++ index.js
@@ -13,3 +13,11 @@ const switchTheme = (e) => {
 };
 
 toggleSwitch.addEventListener('change', switchTheme, false);
+
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+  }
+}
